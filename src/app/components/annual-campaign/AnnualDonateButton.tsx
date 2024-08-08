10b9df57910af36feb5ae2e843fa87478b0c127f
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-08-07 22:24:59 -0700

    added space before futures

--- src/app/components/annual-campaign/AnnualDonateButton.tsx
+++ src/app/components/annual-campaign/AnnualDonateButton.tsx
@@ -1,6 +1,6 @@
 export default function AnnualDonateButton() {
     return (
-        <div className='w-full flex flex-col md:flex-row gap-5 items-center md:items-start pt-5'>
+        <div className='w-full flex flex-col md:flex-row gap-5 items-center md:items-start pt-5 pb-5'>
             <button className='md:flex-1 w-full metallic-button'>
                 <h5>Give Here!</h5>
             </button>
