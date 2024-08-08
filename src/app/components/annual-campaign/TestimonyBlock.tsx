4e712ef2b27f4c9097625679d953117c27832463
kimestelle <137699667+kimestelle@users.noreply.github.com>
2024-08-07 22:36:01 -0700

    changes

--- src/app/components/annual-campaign/TestimonyBlock.tsx
+++ src/app/components/annual-campaign/TestimonyBlock.tsx
@@ -8,7 +8,7 @@ export interface TestimonyProps {
 
 const TestimonyBlock: React.FC<TestimonyProps> = ({ name, testimony, year }) => {
   return (
-    <div className="w-full h-full flex flex-col border-l border-gray-200 p-5 pb-3">
+    <div className="w-full h-full flex flex-col p-5 pb-3">
       <p>&quot; {testimony} &quot;</p>
       <div className="mt-auto text-right dp-red leading-tight">
         <h5 className='font-semibold'>{name}</h5>
